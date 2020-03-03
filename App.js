435b928ad6b67187997c861e3e2d74cb15a70fce
igorWebMASTER <igor_1917@hotmail.com>
2020-03-02 22:23:50 -0300

    home page app

--- App.js
+++ App.js
@@ -1,37 +1,60 @@
-import React, { Component } from 'react';
-import { Platform, View ,  Text, StyleSheet} from 'react-native';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Google Drive</Text>
-        <Text style={styles.welcome}></Text>
-        <View style={styles.boxTop}>
-         <Text></Text>
-        </View>
-      </View>
-    );
-  }
-}
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
 const styles = StyleSheet.create({
-   container: {
-     backgroundColor: '#F5FCFF'
-   },
-   welcome: {
-     fontSize: 30,
-     textAlign: 'left',
-     margin: 40,
-     fontFamily: 'Noto Sans',
-   },
-   boxTop: {
-     width: 400,
-     height: 200,
-     backgroundColor: 'blue',
-     borderRadius: 10,
-     color: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
+  container: {
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'left',
+    margin: 40,
+    fontFamily: 'Noto Sans',
+  },
+  boxTop: {
+    width: 380,
+    height: 200,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    color: '#fff',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 20,
+    marginTop: 20,
+    zIndex: 2,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  boxTopText: {
+    fontSize: 24,
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native</Text>
+      <View style={styles.boxTop}>
+        <Text style={styles.boxTopText}>Abaixo</Text>
+      </View>
+
+      <View style={styles.boxTop}>
+        <Text style={styles.boxTopText}>Abaixo</Text>
+      </View>
+
+      <View style={styles.boxTop}>
+        <Text style={styles.boxTopText}>Abaixo</Text>
+      </View>
+    </View>
+  );
+}
