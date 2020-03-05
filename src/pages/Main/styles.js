0521063a2c95d01052301495bc03888694f09f39
igor ribeiro <igor_1917@hotmail.com>
2020-03-05 17:56:17 -0300

    work

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -13,6 +13,22 @@ export const Form = styled.View`
   border-color: #eee;
 `;
 
-export const Input = styled.TextInput``;
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
 
-export const SubmitButton = styled(RectButton)``;
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
